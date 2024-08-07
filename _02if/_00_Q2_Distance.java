56fe76ffbdfb40f9733507ec8c969e3319e33497
ll <a@a.com>
2024-08-07 12:43:31 +0900

    if

--- _02if/_00_Q2_Distance.java
+++ _02if/_00_Q2_Distance.java
@@ -23,7 +23,40 @@ up1에서 현재 my의 위치가 가까운 쪽 기준으로 출력한다
 		 * 
 		 * 
 		 */
+		int a = 10;
+		int b = 20;
+		int my = 18;
+		
+		//up1
+		 if( a <= my && my <= b) {
+			 System.out.println("도로 위");
+		 }else {
+			 System.out.println("도로가 아님");
+		 }
+		//up2
+		 if( a <= my && my <= b) {
+			 System.out.println("도로 위");
+			 int distanceCenter = a+(b-a)/2;
+			 System.out.println(distanceCenter);
+			 if(my <= distanceCenter) {
+				 System.out.println("a로 부터 "+(my-a));
+			 }else {
+				 System.out.println("b로 부터 "+(b-my));
+			 }
+		 }else {
+			 System.out.println("도로가 아님");
+		 }
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
