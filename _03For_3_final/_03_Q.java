e1893cb53833e369a59876fe9978f3ed9ff5b954
ll <a@a.com>
2024-08-14 11:43:50 +0900

    random scanner

--- _03For_3_final/_03_Q.java
+++ _03For_3_final/_03_Q.java
@@ -8,8 +8,6 @@ int[] letter={8,12,3,13,1,14,3,4};
 letter  배열은 word문자열 알파벳의 위치이다.
 letter배열의 암호를 풀어서 편지의 내용을 출력하시오. 
  * 
- * 
- * @param args
  */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
