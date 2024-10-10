2e3eba34cc4fbbc8c2443f250c34aad011aa09eb
kimtr <nagneoo@naver.com>
2024-10-10 11:32:55 +0900

    idea

---
+++ _22_main/Main.java
@@ -0,0 +1,12 @@
+package _22_main;
+
+import _22_service.IdeaService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new IdeaService();
+	}
+
+}
